8b63789b9b9d23ad78d7640a426aeadd4832b891
Juan Pablo Velez Amaya <jpva360@gmail.com>
2026-04-18 22:47:03 -0500

    actu

--- app/src/main/java/com/deltaforce/coingrade/MainActivity.java
+++ app/src/main/java/com/deltaforce/coingrade/MainActivity.java
@@ -2,11 +2,7 @@ package com.deltaforce.coingrade;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        bnActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         NavHostFragment navHost = (NavHostFragment) getSupportFragmentManager()
